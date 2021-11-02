66a1274af5edacbc51c4739f4b41ac36c8e863f5
ailo21 <alex92ailo@gmail.com>
2021-11-02 22:28:02 +0300

    add feature use keyboard in calculator

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,20 @@ import NumBtn from "./components/NumBtn";
 import OperationBtn from "./components/OperationBtn";
 import {operationEnum} from "./types/types";
 import {Input} from 'antd'
-import { CloseOutlined,MinusOutlined,PlusOutlined } from '@ant-design/icons';
+import {CloseOutlined, MinusOutlined, PlusOutlined} from '@ant-design/icons';
 
 function App() {
     const [firstNum, setFirstNum] = useState<number | undefined>();
     const [selectOperation, setSelectOperation] = useState<operationEnum | null>(null);
     const [secondNum, setSecondNum] = useState<number | undefined>();
     const [displayText, setDisplayText] = useState('');
+    const inputEl = React.useRef<Input>(null);
 
+    useEffect(()=>{
+        if (inputEl.current){
+            inputEl.current.focus()
+        }
+    },[]);
     const clickHandlerNum = (num: number) => {
         if (!selectOperation) {
             if (firstNum === undefined) {
@@ -52,7 +58,8 @@ function App() {
                     result = firstNum! / secondNum!;
                     break;
                 default:
-                    throw new Error('Такой операции не существует');
+                    // throw new Error('Такой операции не существует');
+                    console.log('Такой операции не существует')
                     break;
             }
             setFirstNum(result);
@@ -63,6 +70,56 @@ function App() {
             setSelectOperation(operation);
         }
     }
+
+    function handleKeyPress(event: React.KeyboardEvent): any {
+
+        switch (event.key) {
+            case 'Backspace':
+                Backspace()
+                break;
+            case '+':
+                clickHandlerOperation(operationEnum.fold)
+                break;
+            case '-':
+                clickHandlerOperation(operationEnum.subtract)
+                break;
+            case '*':
+                clickHandlerOperation(operationEnum.multiply)
+                break;
+            case '/':
+                clickHandlerOperation(operationEnum.division)
+                break;
+            case 'Enter':
+                clickHandlerOperation(operationEnum.equals)
+                break;
+            case '=':
+                clickHandlerOperation(operationEnum.equals)
+                break;
+            default:
+                debugger;
+                break;
+        }
+
+        const regExpNum = new RegExp(/\d{1}/);
+        if (regExpNum.test(event.key)) {
+            clickHandlerNum(Number(event.key));
+        }
+    }
+
+    function Backspace() {
+        if (secondNum !== undefined) {
+            let arr = secondNum.toString().split('');
+            arr.pop();
+            setSecondNum(Number(arr.join('')));
+        } else if (selectOperation !== null) {
+            setSelectOperation(null);
+        } else if (firstNum !== undefined) {
+            let arr = firstNum.toString().split('');
+            arr.pop();
+            setFirstNum(Number(arr.join('')));
+        }
+    }
+
     useEffect(() => {
         setDisplayText(`${firstNum ?? ''}${selectOperation ?? ''}${secondNum ?? ''}`);
     }, [firstNum, selectOperation, secondNum])
@@ -70,7 +127,7 @@ function App() {
     return (
         <div className="App">
             <div className={"result_input"}>
-                <Input  readOnly value={displayText} type={"text"}/>
+                <Input ref={inputEl} onKeyUp={handleKeyPress} value={displayText} type={"text"}/>
             </div>
 
             <div className={"row"}>
